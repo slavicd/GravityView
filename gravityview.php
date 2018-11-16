c40d8e6ab59dd9cbb83dc5a4adb3cf62b58c9379
Slavic Dragovtev <slavic@entropi.me>
2018-11-16 20:43:04 +0200

    version bump

--- gravityview.php
+++ gravityview.php
@@ -3,7 +3,7 @@
  * Plugin Name:       	GravityView
  * Plugin URI:        	https://gravityview.co
  * Description:       	The best, easiest way to display Gravity Forms entries on your website.
- * Version:          	2.1.1
+ * Version:          	2.1.1.1
  * Author:            	GravityView
  * Author URI:        	https://gravityview.co
  * Text Domain:       	gravityview
